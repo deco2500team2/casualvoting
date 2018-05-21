cbee56d8c197245b54e5d6b3551f9e0313eaaa73
Simon Curtis <scurtis142@gmail.com>
2018-05-21 23:00:03 +1000

    added id's to controller

--- src/Source/Controller.java
+++ src/Source/Controller.java
@@ -13,12 +13,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
- * Controller class for the Character Editor.
- * @author Alexandra Crawley
- * @author leggy (Lachlan Healey)
+ * Controller class for the voting application.
+ * @author Simon Curtis
  */
 public class Controller implements Initializable{
 	/*
@@ -32,6 +34,73 @@ public class Controller implements Initializable{
 	private Button login$loginbutton;
 	@FXML
 	private Button login$registerButton;
+	@FXML
+	private TextField login$usernameTextField;
+	@FXML
+	private TextField login$passwordTextField;
+	@FXML
+	private ComboBox login$titleDropdown;
+	@FXML
+	private TextField login$firstnameTextField;
+	@FXML
+	private TextField login$surnameTextField;
+	@FXML
+	private TextField login$dobTextField;
+	@FXML
+	private TextField login$emailTextField;
+	@FXML
+	private TextField login$otherUsernameTextField;
+	@FXML
+	private TextField login$otherPasswordTextField;
+	@FXML
+	private TextField login$repeatPasswordTextField;
+	@FXML
+	private Button homeButton;
+	@FXML
+	private Button logoutButton;
+	@FXML
+	private Button backButton;
+	@FXML
+	private Button home$accountDetailsButton;
+	@FXML
+	private Button home$createVoteButton;
+	@FXML
+	private Button home$viewVoteAndDetailsButton;
+	@FXML
+	private ListView home$recommendedVotesListView;
+	@FXML
+	private ComboBox accountDetails$titleDropdown;
+	@FXML
+	private Button accountDetails$saveButton;
+	@FXML
+	private Button editVote$saveButton;
+	@FXML
+	private Button editVote$addQuestionButton;
+	@FXML
+	private Button editVote$viewQuestionButton;
+	@FXML
+	private Button editVote$deleteQuestionButton;
+	@FXML
+	private TextField editVote$voteTitleTextField;
+	@FXML
+	private TextField editVote$descriptionTextField;
+	@FXML
+	private Button createVote$saveButton;
+	@FXML
+	private Button createVote$addQuestionButton;
+	@FXML
+	private Button createVote$viewQuestionButton;
+	@FXML
+	private Button createVote$deleteQuestionButton;
+	@FXML
+	private TextField createVote$voteTitleTextField;
+	@FXML
+	private TextField createVote$descriptionTextField;
+	
+	
+	
+	
+	
 	
 	/**
 	 * A constructor to initialise the controller with no parameters.
